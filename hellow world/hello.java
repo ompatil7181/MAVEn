12225e5a4644a6d5bbdbbbcc589fffa96ea49f13
omkar <omkargit config --global user.name omkargit config --global user.email omkar>
2026-02-15 09:17:30 +0530

    addition

--- hellow world/hello.java
+++ hellow world/hello.java
@@ -1,5 +1,11 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println( "Hello World");
+    
+        addition(5, 10);
+        
+    }
+    public static void addition(int a, int b) {
+        System.out.println(a + b);
     }
 }
